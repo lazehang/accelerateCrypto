6c91afcfdf8cead7aa2324a872775d4fa47e9155
Nate <nate@natewelling.com>
2018-06-09 15:26:32 +0700

    add sample-coins.js

---
+++ src/sample-coins.js
@@ -0,0 +1,75 @@
+const coins = {
+  coin1: {
+    name: 'Bitcoin',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/240px-Bitcoin.svg.png',
+    desc: 'The original cryptocurrency, digital gold.',
+    price: 7667,
+    status: 'available'
+  },
+
+  coin2: {
+    name: 'Ethereum',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/256px-Ethereum_logo_2014.svg.png',
+    desc: 'Programmable contracts and money.',
+    price: 606,
+    status: 'available'
+  },
+
+  coin3: {
+    name: 'Bitcoin Cash',
+    image: 'https://i.redd.it/nus982esrz901.png',
+    desc: 'Bitcoin clone.',
+    price: 1126,
+    status: 'available'
+  },
+
+  coin4: {
+    name: 'Litecoin',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/e/e3/Litecoin_Logo.jpg',
+    desc: 'Fork of Bitcoin, faster.',
+    price: 120,
+    status: 'available'
+  },
+
+  coin5: {
+    name: 'IOTA',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Iota_logo.png/320px-Iota_logo.png',
+    desc: 'Internet-of-things payments.',
+    price: 2,
+    status: 'available'
+  },
+
+  coin6: {
+    name: 'NEO',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/NEO_%28cryptocurrency%29_logo.svg/240px-NEO_%28cryptocurrency%29_logo.svg.png',
+    desc: 'Chinese-market Ethereum.',
+    price: 53,
+    status: 'available'
+  },
+
+  coin7: {
+    name: 'Monero',
+    image: 'https://news.bitcoin.com/wp-content/uploads/2016/09/monero-r-500x500.jpg',
+    desc: 'Private digital cash.',
+    price: 159,
+    status: 'available'
+  },
+
+  coin8: {
+    name: 'Dash',
+    image: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Dash_%28cryptocurrency%29_logo.svg/320px-Dash_%28cryptocurrency%29_logo.svg.png',
+    desc: 'Privacy-focused Bitcoin clone.',
+    price: 300,
+    status: 'available'
+  },
+
+  coin9: {
+    name: 'VeChain',
+    image: 'https://www.coinspot.com.au/public/img/coinlg/vechain.png?v=79',
+    desc: 'Supply chain item IDs.',
+    price: 4,
+    status: 'available'
+  }
+};
+
+export default coins;
